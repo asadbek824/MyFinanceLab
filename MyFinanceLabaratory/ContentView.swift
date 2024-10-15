386b8c1561c10c9eb99538098e9a5ea9932e411d
asadbek824 <yoldoshev5007@icloud.com>
2024-10-15 11:16:28 +0500

    Initial Commit

---
+++ MyFinanceLabaratory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFinanceLabaratory
+//
+//  Created by Asadbek Yoldoshev on 15/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFinanceLabaratory/MyFinanceLabaratoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceLabaratoryApp.swift
+//  MyFinanceLabaratory
+//
+//  Created by Asadbek Yoldoshev on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceLabaratoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
